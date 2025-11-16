fece9a5f1512788b0a9706a69e8848978a99b19d
yixiaojiu <yi_xiao_jiu@qq.com>
2025-11-17 01:33:47 +0800

    build: close auto restart

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   apps: [
     {
-      name: "yuiju-message",
-      script: "pnpm",
-      args: "run start:message",
+      name: 'yuiju-message',
+      script: 'pnpm',
+      args: 'run start:message',
       cwd: __dirname,
       env: {
-        NODE_ENV: "production"
+        NODE_ENV: 'production',
       },
-      autorestart: true,
+      autorestart: false,
       watch: false,
-      max_memory_restart: "512M"
-    }
-  ]
-}
+      max_memory_restart: '512M',
+    },
+  ],
+};
